c7c97748a442306081bc5df52a589a3bc24242e1
ju-to <088t3748@gmail.com>
2020-10-26 06:02:53 +0900

    レビューを受けての修正

--- kadai.rb
+++ kadai.rb
@@ -1,7 +1,7 @@
 require "csv"
 
 plans = CSV.read("travel_plan.csv", headers: true).map do |row|
-  { id: row["id"], place: row["place"], price: row["price"] }
+  { id: row["id"].to_i, place: row["place"], price: row["price"] }
 end
 
 def disp_plan(plans)
@@ -12,19 +12,21 @@ def disp_plan(plans)
   print "プランを選択 > "
 end
 
-def disp_choice_plan(plan_number, plans)
+def disp_choice_plan(plans)
+  plan_number = gets.to_i
   while plan_number > plans.size || plan_number <= 0
     puts "1〜#{plans.size}の間で入力し直してください"
     plan_number = gets.to_i
   end
-  plan = plans.find { |plan| plan[:id].to_i == plan_number }
+  plan = plans.find { |plan| plan[:id] == plan_number }
   puts "#{plan[:place]}旅行ですね、何人で行きますか？"
   plan
 end
 
-def member(number_of_people)
+def member
+  number_of_people = gets.to_i
   while number_of_people <= 0
-    puts "人数は0人以上にして下さい"
+    puts "人数は1人以上にして下さい"
     number_of_people = gets.to_i
   end
   number_of_people
@@ -41,9 +43,7 @@ def disp_total_price(price, number_of_people)
 end
 
 disp_plan(plans)
-plan_number = gets.to_i
-choice_plan = disp_choice_plan(plan_number, plans)
-number_of_people = gets.to_i
-input_member = member(number_of_people)
+choice_plan = disp_choice_plan(plans)
+input_member = member
 total_price = total_price(choice_plan, input_member)
 disp_total_price(total_price, input_member)
